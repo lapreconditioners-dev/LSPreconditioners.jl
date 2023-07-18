98df7b648831be3ee417ab0121a8e15cc0208765
npritch928 <npritchard@wisc.edu>
2023-07-18 17:59:48 -0500

    Made corrections based on comments for blockJacobi

--- src/spai.jl
+++ src/spai.jl
@@ -3,48 +3,44 @@ using SparseArrays
 
 #Implementation of SPAI preconditoner which allows one to specify a sparsity pattern stored in a matrix Z and it formulates a preconditioner that fits into the sparisty pattern
 
-mutable struct SPAI{T, S<:AbstractMatrix{T}} <: Preconditioner
+mutable struct SPAI{T, S<:AbstractMatrix{T}} <: LSPreconditioners.Preconditioner
     M::S
 end
-function SPAI(A::AbstractMatrix,Z::SparseMatrixCSC)
-    m = size(A,1)
-    T = eltype(A)
-    nz = nnz(Z)
-    v = Array{T}(undef,nz)
-    nzr,nzc,_ = findnz(Z)
-    M = sparse(nzr,nzc,v)
-    Id = zeros(m)
-    for i = 1:m
-        Id[i] = 1
-        rdx = nzr[nzc .== i]
-        M[rdx,i] = A[:,rdx]\Id#ldiv!(M[rdx,i],qr(A[:,rdx]),Id)
-        Id[i] = 0
-    end
-    return SPAI{eltype(A),typeof(A)}(M)
-end
 
-function SPAI(A::AbstractMatrix,Z::BandedMatrix)
-    m = size(A,1)
+Base.eltype(::SPAI{T, S}) where {T, S} = T
+
+function SPAI(A::AbstractMatrix, Z::Union{SparseMatrixCSC, BandedMatrix})
+    m = size(A, 1)
     T = eltype(A)
-    nz = nnz(Z)
-    (u,l) = bandwidths(Z)
-    M = BandedMatrix{T}(undef,(m,m),(u,l))
     Id = zeros(m)
-    for i = 1:m
-        Id[i] = 1
-        if i>u && i<m-l
-            rdx = i-u:i+l
-        elseif i <= u
-            rdx = 1:i+l
-        else
-            rdx = i-u:m
+    if typeof(Z) <: BandedMatrix
+        (u, l) = bandwidths(Z)
+        for i in 1:m
+            Id[i] = 1
+            if i > u && i < m-l
+                rdx = i-u:i+l
+            elseif i <= u
+                rdx = 1:i+l
+            else
+                rdx = i-u:m
+            end
+
+            @views Z[rdx, i] .= qr(A[:, rdx]) \ Id
+            Id[i] = 0
         end
-         M[rdx,i] = A[:,rdx]\Id#ldiv!(M[rdx,i],qr(A[:,rdx]),Id)
-        Id[i] = 0
+    else 
+        nzr, nzc, _ = findnz(Z)
+        for i in 1:m
+            Id[i] = 1
+            rdx = nzr[nzc .== i]
+            @views Z[rdx, i] .= qr(A[:, rdx]) \ Id
+            Id[i] = 0
+        end
     end
-    return SPAI{eltype(A),typeof(A)}(M)
+
+    return SPAI{eltype(A), typeof(A)}(Z)
 end
 
-function LinearAlgebra.mul!(x,P::SPAI,y)
-    mul!(x,P.M,y)
+function LinearAlgebra.mul!(x, P::SPAI, y)
+    mul!(x, P.M, y)
 end
